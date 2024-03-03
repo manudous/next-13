b6b8f4c8e30e46342bd47e48c0d3f0a8a110c94b
manudous <manudous@gmail.com>
2024-03-03 19:05:54 +0100

    add count page

---
+++ 02-my-dashboard/src/app/dashboard/counter/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Metadata } from "next";
+import { CartCounter } from "@/app/shopping-cart";
+
+export const metadata: Metadata = {
+  title: "Shopping cart",
+  description: "Un simple contador",
+  keywords: "contador, carrito, productos",
+};
+
+export default function CounterPage() {
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Productos en el carrito</span>
+      <CartCounter value={20} />
+    </div>
+  );
+}
